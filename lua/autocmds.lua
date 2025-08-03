94167736d778ee645f6c7f57e7d9f0b4ddb241a2
Hao Ren Zhou <hrz9009@gmail.com>
2025-08-03 22:24:47 +0800

    add autocmd for nvimtree on launch

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -20,6 +20,18 @@ autocmd("BufDelete", {
   end,
 })
 
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  desc = "Open Nvdash on startup only if no other buffers are loaded",
+  callback = function()
+    local bufs = vim.api.nvim_list_bufs()
+    local buf_name = vim.api.nvim_buf_get_name(bufs[1])
+
+    if #bufs == 1 and buf_name == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
+
 autocmd({ "QuitPre" }, {
   desc = "Close NvimTree on quit",
   callback = function()
